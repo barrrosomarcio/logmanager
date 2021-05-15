cba9d7204bf7ae506656478c2b26828a70d70563
Marcio <marcio@localhost.localdomain>
2021-05-15 18:04:58 -0300

    tela about pronta

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ JavaBugs
 
 #bibliotecas:
   npm install react-social-icons
+  https://github.com/jaketrent/react-social-icons
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import Andre from '../../img/andre.png';
+import Brune from '../../img/brune.jpeg';
+import Marcio from '../../img/marcio.jpeg';
+import Rafa from '../../img/rafa.JPG';
 import Header from '../../components/header';
-import Pessoa from '../../img/pessoa.jpeg';
+import Jesssica from '../../img/jessica.png';
 import './about.css';
 
 const About = () => {
@@ -10,36 +15,49 @@ const About = () => {
       <section className="container">
         <h1 className="title" >Integrantes:</h1>
         <div className="card">
-          <img src={ Pessoa } alt="pessoa"/>
+          <img src={ Andre } alt="pessoa"/>
           <h3>André Horman</h3>
-          <a>
-            linkedin
-          </a>
-          <button>github</button>
+          <p>andrehormandev@gmail.com</p>
+          <div>
+            <SocialIcon url="https://www.linkedin.com/in/andrehorman/" network="linkedin"/>
+            <SocialIcon url="https://github.com/ANDREHORMAN1994" network="github" />
+          </div>
         </div>
         <div className="card">
-          <img src={ Pessoa } alt="pessoa"/>
+          <img src={ Brune } alt="pessoa"/>
           <h3>Brune Miguel Paes Freire</h3>
-          <button>linkedin</button>
-          <button>github</button>
+          <p>bstfreire@gmail.com</p>
+          <div>
+            <SocialIcon url="https://www.linkedin.com/in/brunefreire/" network="linkedin"/>
+            <SocialIcon url="https://github.com/Brune-Freire" network="github" />
+          </div>
         </div>
         <div className="card">
-          <img src={ Pessoa } alt="pessoa"/>
+          <img src={ Jesssica } alt="pessoa"/>
           <h3>Jéssica Landislau</h3>
-          <button>linkedin</button>
-          <button>github</button>
+          <p>paula.jfe@gmail.com</p>
+          <div>
+            <SocialIcon url="https://www.linkedin.com/in/jessica-ladislau/" network="linkedin"/>
+            <SocialIcon url="https://github.com/paula-jfe" network="github" />
+          </div>
         </div>
         <div className="card">
-          <img src={ Pessoa } alt="pessoa"/>
+          <img src={ Marcio } alt="pessoa"/>
           <h3>Márcio Barroso</h3>
-          <button>linkedin</button>
-          <button>github</button>
+          <p>barrrosomarcio@gmail.com</p>
+          <div>
+            <SocialIcon url="https://www.linkedin.com/in/márcio-barroso-569234181" network="linkedin"/>
+            <SocialIcon url="https://github.com/barrrosomarcio" network="github" />
+          </div>
         </div>
         <div className="card">
-          <img src={ Pessoa } alt="pessoa"/>
+          <img src={ Rafa } alt="pessoa"/>
           <h3>Rafael Machado Guimarães</h3>
-          <button>linkedin</button>
-          <button>github</button>
+          <p>rafaelmguimaraes@gmail.com</p>
+          <div>
+            <SocialIcon url="https://www.linkedin.com/in/rafaelmguimaraes" network="linkedin"/>
+            <SocialIcon url="https://github.com/rafaelmguimaraes" network="github" />
+          </div>
         </div>
       </section>
     </div>
