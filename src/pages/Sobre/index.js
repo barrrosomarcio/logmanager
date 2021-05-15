f09b372da69d09513e5142b85c612e58d0d74271
Marcio <marcio@localhost.localdomain>
2021-05-15 18:31:54 -0300

    ajustes tela about

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -18,7 +18,7 @@ const About = () => {
           <img src={ Andre } alt="pessoa"/>
           <h3>André Horman</h3>
           <p>andrehormandev@gmail.com</p>
-          <div>
+          <div className="social">
             <SocialIcon url="https://www.linkedin.com/in/andrehorman/" network="linkedin"/>
             <SocialIcon url="https://github.com/ANDREHORMAN1994" network="github" />
           </div>
@@ -27,16 +27,16 @@ const About = () => {
           <img src={ Brune } alt="pessoa"/>
           <h3>Brune Miguel Paes Freire</h3>
           <p>bstfreire@gmail.com</p>
-          <div>
+          <div className="social">
             <SocialIcon url="https://www.linkedin.com/in/brunefreire/" network="linkedin"/>
             <SocialIcon url="https://github.com/Brune-Freire" network="github" />
           </div>
         </div>
         <div className="card">
           <img src={ Jesssica } alt="pessoa"/>
-          <h3>Jéssica Landislau</h3>
+          <h3>Jessica Ladislau</h3>
           <p>paula.jfe@gmail.com</p>
-          <div>
+          <div className="social">
             <SocialIcon url="https://www.linkedin.com/in/jessica-ladislau/" network="linkedin"/>
             <SocialIcon url="https://github.com/paula-jfe" network="github" />
           </div>
@@ -45,7 +45,7 @@ const About = () => {
           <img src={ Marcio } alt="pessoa"/>
           <h3>Márcio Barroso</h3>
           <p>barrrosomarcio@gmail.com</p>
-          <div>
+          <div className="social">
             <SocialIcon url="https://www.linkedin.com/in/márcio-barroso-569234181" network="linkedin"/>
             <SocialIcon url="https://github.com/barrrosomarcio" network="github" />
           </div>
@@ -54,7 +54,7 @@ const About = () => {
           <img src={ Rafa } alt="pessoa"/>
           <h3>Rafael Machado Guimarães</h3>
           <p>rafaelmguimaraes@gmail.com</p>
-          <div>
+          <div className="social">
             <SocialIcon url="https://www.linkedin.com/in/rafaelmguimaraes" network="linkedin"/>
             <SocialIcon url="https://github.com/rafaelmguimaraes" network="github" />
           </div>
