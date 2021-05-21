601851a6c5a8b378bb5d0978b89403ebb66351db
Marcio <marcio@localhost.localdomain>
2021-05-21 18:07:41 -0300

    login

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import qs from 'querystring';
+import base64 from 'base-64';
+
 
 const PORT = process.env.PORT;
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -8,59 +10,30 @@ const endpoint = `http://localhost:3050/oauth/token`;
 
 
 export const loginAPI = async (email, password) => {
-  let formData = new FormData();
-  formData.append('grant_type', 'password');
-  formData.append('username', email);
-  formData.append('password', password);
-  // formData.append('client_id', CLIENT_ID);
-  // formData.append('client_secret', SECRET);
-  // formData.append('scope', 'password');
-  // const body = {
-  //   "grant_type": 'password',
-  //   "username": email,
-  //   "password": password,
-  //   "client_id": CLIENT_ID,
-  //   "client_secret": SECRET,
-  //   "scope": "password"
-  // };
-  // const options = {
-  //   method: 'POST',
-  //   Headers: {
-  //     'content-type': 'application/x-www-form-urlencoded'
-  //   },
-  //   data: qs.stringify(body),
-  //   url: endpoint,
-  // }
-  // axios(options).then(res => console.log(res))var session_url = 'http://api_address/api/session_endpoint';
-      const appusername = CLIENT_ID;
-      const apppassword = SECRET;
-      // const credentials = btoa(appusername + ':' + apppassword);
-      // const basicAuth = 'Basic ' + credentials;
-      // axios.post(endpoint, {
-      //   auth: {
-      //     'username': appusername,
-      //     'password': apppassword,
-      //   },
-      //   headers: {  'content-type': 'multipart/form-data' },
-      //   body: formData,
-      // }).then(function(response) {
-      //   console.log('Authenticated');
-      // }).catch(function(error) {
-      //   console.log('Error on Authentication');
-      // });
+  
+  const FormData = require('form-data');
+  const data = new FormData();
+  data.append('grant_type', 'password');
+  data.append('username', 'teste@teste.com');
+  data.append('password', '123456');
 
-      fetch(endpoint, {
-        method: 'POST',
-        headers: {
-          'content-type': 'multipart/form-data',
-          'Access-Control-Allow-Origin:': '*', 
-        },
-        body: formData,
-        auth: {
-          'username': appusername,
-          'password': apppassword,
-        },
-        }).then(response => {console.log(response)}).catch(error => { console.log(error)})
+  const config = {
+    method: 'post',
+    url: 'http://localhost:3050/oauth/token',
+    headers: { 
+      'Authorization': 'Basic bG9nbWFuYWdlcjoxMjM=', 
+      'Content-Type': 'form-data',
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 };
 
 // let formData = new FormData();
